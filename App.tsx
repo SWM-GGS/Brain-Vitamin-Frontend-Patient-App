aafce4cddb5ceb29a7971824782934f20c19a4c4
salmonco <xkqjsslsek80@naver.com>
2023-08-11 18:42:48 +0900

    Feat: android physical back button work on webview

--- App.tsx
+++ App.tsx
@@ -1,21 +1,72 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Dimensions, SafeAreaView, StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { BackHandler } from 'react-native';
+import DismissKeyboardView from './src/components/DismissKeyboardView';
 
 const deviceHeight = Dimensions.get('window').height;
 const deviceWidth = Dimensions.get('window').width;
 
 function App(): JSX.Element {
+  const webview = useRef<WebView>(null);
+  const [isCanGoBack, setIsCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const onPressHardwareBackButton = () => {
+      if (webview.current && isCanGoBack) {
+        webview.current.goBack();
+        return true;
+      } else {
+        return false;
+      }
+    };
+    BackHandler.addEventListener(
+      'hardwareBackPress',
+      onPressHardwareBackButton,
+    );
+    return () => {
+      BackHandler.removeEventListener(
+        'hardwareBackPress',
+        onPressHardwareBackButton,
+      );
+    };
+  }, [isCanGoBack]);
+
   return (
     <SafeAreaView style={styles.container}>
-      <WebView
-        style={styles.webview}
-        // SSL 없는 주소의 경우, 터미널에 adb reverse tcp:5173 tcp:5173 입력
-        source={{ uri: 'http://localhost:5173/oneToFifty' }}
-        // source={{ uri: 'https://naver.com' }}
-        javaScriptEnabled={true}
-        domStorageEnabled={true}
-      />
+      <DismissKeyboardView>
+        <WebView
+          ref={webview}
+          style={styles.webview}
+          // SSL 없는 주소의 경우, 터미널에 adb reverse tcp:5173 tcp:5173 입력
+          source={{ uri: 'http://localhost:5173' }}
+          injectedJavaScript={`
+        (function() {
+          function wrap(fn) {
+            return function wrapper() {
+              var res = fn.apply(this, arguments);
+              window.ReactNativeWebView.postMessage('navigationStateChange');
+              return res;
+            }
+          }
+    
+          history.pushState = wrap(history.pushState);
+          history.replaceState = wrap(history.replaceState);
+          window.addEventListener('popstate', function() {
+            window.ReactNativeWebView.postMessage('navigationStateChange');
+          });
+        })();
+    
+        true;
+      `}
+          onMessage={({ nativeEvent: state }) => {
+            if (state.data === 'navigationStateChange') {
+              // Navigation state updated, can check state.canGoBack, etc.
+              setIsCanGoBack(state.canGoBack);
+            }
+          }}
+        />
+      </DismissKeyboardView>
     </SafeAreaView>
   );
 }
